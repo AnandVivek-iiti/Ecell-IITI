fde0d3c84da4a3b94dc25c93fa64d43ef8d4e149
mahisingh06330-arch <mahisingh06330@gmail.com>
2026-06-01 19:02:03 +0530

    Event page updated

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function AppContent() {
       <ScrollToTop />
       <Navbar />
 
-      <main className={`flex-grow py-0 route-transition ${pageReady ? 'page-visible' : ''}`}>
+      <main className={`flex-grow px-4 py-0 sm:px-6 lg:px-8 route-transition ${pageReady ? 'page-visible' : ''}`}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
