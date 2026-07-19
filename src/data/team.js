987041411a94f843f197fe406a10e9c1ea8b27c5
Harshk013 <harshkasera262@gmail.com>
2026-07-19 13:35:56 +0530

    Create team.js

---
+++ src/data/team.js
@@ -0,0 +1,489 @@
+const TEAM_DATA = {
+  "board": [
+    {
+      "name": "Dr. Swaminathan R.",
+      "position": "CEO IITI ACE Foundation",
+      "initials": "SR",
+      "photo": "images/Team/Dr.Swaminathan.jpg",
+      "linkedin": "/team",
+      "instagram": "/team"
+    },
+    {
+      "name": "Mr. Rajeev Pandey",
+      "position": "CEO IITI ACE Foundation",
+      "initials": "RP",
+      "photo": "images/Team/Rajeev_Pandey.jpg",
+      "linkedin": "/team",
+      "instagram": "/team"
+    },
+    {
+      "name": "Sonal Rai",
+      "position": "Junior Assistant",
+      "initials": "SR",
+      "photo": "images/Team/Sonal_Rai.jpeg",
+      "linkedin": "/team",
+      "instagram": "/team"
+    }
+  ],
+  "secretaries": [
+    {
+      "name": "Khushi Sawla",
+      "position": "E-Cell Secretary",
+      "initials": "KS",
+      "photo": "images/Team/khushi_sawla.png",
+      "linkedin": "https://www.linkedin.com/in/khushi-sawla-303458222",
+      "instagram": "https://instagram.com/khushiisawlaa?igshid=OGQ5ZDc2ODk2ZA=="
+    },
+    {
+      "name": "Ayush Shukla",
+      "position": "E-Cell Joint-Secretary",
+      "initials": "AS",
+      "photo": "images/Team/photo - Ayush_Shukla.jpeg",
+      "linkedin": "https://www.linkedin.com/in/ayush-shukla-iiti5011/",
+      "instagram": "https://www.instagram.com/ayush_s746/"
+    }
+  ],
+  "leaders": [
+    {
+      "name": "Manya Rajib Jain",
+      "position": "Marketing",
+      "initials": "MJ",
+      "photo": "images/marketing/Manya_Rajib_Jain.png",
+      "linkedin": "https://www.linkedin.com/in/manya-rajib-jain-7a972a22b",
+      "instagram": "https://instagram.com/manyarajib?igshid=MzRlODBiNWFlZA=="
+    },
+    {
+      "name": "Chirag Gour",
+      "position": "Outreach",
+      "initials": "CG",
+      "photo": "images/Outreach/Chirag_Gour.jpeg",
+      "linkedin": "https://www.linkedin.com/in/chirag-gour-a58941232",
+      "instagram": "https://instagram.com/_chirag__17?igshid=NGExMmI2YTkyZg=="
+    },
+    {
+      "name": "Aditi Ganvir",
+      "position": "Technicals",
+      "initials": "AG",
+      "photo": "images/Technicals/Aditi_Ganvir.jpeg",
+      "linkedin": "https://www.linkedin.com/in/aditi-ganvir-216336232/",
+      "instagram": "https://www.instagram.com/ganviraditi/"
+    },
+    {
+      "name": "Nivedita",
+      "position": "Content and Social Media",
+      "initials": "N",
+      "photo": "images/Content/me - Nivedita Iiti.jpg",
+      "linkedin": "https://www.linkedin.com/in/nivedita-376271227/",
+      "instagram": "https://www.instagram.com/nive_jha/"
+    },
+    {
+      "name": "Vyankatesh Chavan",
+      "position": "Creatives",
+      "initials": "VC",
+      "photo": "images/Video_Production_Creative/Vyankatesh Chavan.jpg",
+      "linkedin": "https://www.linkedin.com/in/vyankatesh-chavan-bbb383230",
+      "instagram": "https://instagram.com/vyankatesh_chavan_?igshid=NGExMmI2YTkyZg=="
+    },
+    {
+      "name": "Pranjal Jaiswal",
+      position: "Video Production",
+      initials: "PJ",
+      photo: "images/Video_Production_Creative/Pranjal_Jaiswal.jpeg",
+      "linkedin": "https://www.linkedin.com/in/pranjal-jaiswal-37a12a201",
+      "instagram": "https://instagram.com/pranjal._.jaiswal?igshid=MjEwN2IyYWYwYw=="
+    },
+    {
+      "name": "Ravi Kumar",
+      position: "Operation & Logistic",
+      initials: "RK",
+      photo: "images/onl/ravi_pratap.jpg",
+      "linkedin": "https://www.linkedin.com/in/ravi-kumar-a7abb6247",
+      "instagram": "https://instagram.com/ravipratap80053?igshid=ZDc4ODBmNjlmNQ=="
+    }
+  ],
+  "teams": {
+    "Marketing": [
+      {
+        "name": "Atharva Dhore",
+        "initials": "AD",
+        "photo": "images/Marketing/atharva.jpg",
+        "linkedin": "https://www.linkedin.com/in/atharva-dhore-b9502b23b",
+        "instagram": "https://instagram.com/atharva_dhore7?igshid=OGQ5ZDc2ODk2ZA"
+      },
+      {
+        "name": "Manisha Sahu",
+        "initials": "MS",
+        "photo": "images/Marketing/20230328_171750 - Manisha Devi.jpg",
+        "linkedin": "https://www.linkedin.com/in/manisha-sahu-5575b9205/",
+        "instagram": "https://instagram.com/mani_shail"
+      },
+      {
+        "name": "Aniket Tiwari",
+        "initials": "AT",
+        "photo": "images/Marketing/IMG_20230525_133942 - Aniket Tiwari.jpg",
+        "linkedin": "https://www.linkedin.com/in/aniket6012",
+        "instagram": "https://instagram.com/tiwa.anni__?igshid=NGExMmI2YTkyZg=="
+      },
+      {
+        "name": "Raina Tathed",
+        "initials": "RT",
+        "photo": "images/Marketing/Raina_Tathed - Raina Tathed.jpg",
+        "linkedin": "https://www.linkedin.com/in/raina-tathed-5650a025b",
+        "instagram": "https://instagram.com/raina_tathed?igshid=ZDc4ODBmNjlmNQ=="
+      },
+      {
+        "name": "Yash Baghel",
+        "initials": "YB",
+        "photo": "images/Marketing/IMG_20221223_002959_690 - Yash Baghel.jpg",
+        "linkedin": "https://www.linkedin.com/in/yash-baghel-3435b525b/",
+        "instagram": "https://instagram.com/shadowchaser_04?igshid=MzNlNGNkZWQ4Mg=="
+      },
+      {
+        "name": "Dhruv Dhirawani",
+        "initials": "DD",
+        "photo": "images/Marketing/IMG_20230607_224855 - Dhruv Dhirawani.png",
+        "linkedin": "https://www.linkedin.com/in/dhruv-dhirawani-8b6b9225a/",
+        "instagram": "https://www.instagram.com/dhruv_dhirawani/?hl=en"
+      },
+      {
+        "name": "Shivam Sharma",
+        "initials": "SS",
+        "photo": "images/Marketing/WhatsApp Image 2023-06-07 at 23.31.06 - Shivam Sharma.jpg",
+        "linkedin": "https://www.linkedin.com/in/shivam-sharma-b044b1255/",
+        "instagram": "https://www.instagram.com/"
+      },
+      {
+        "name": "Kanika Upadhyay",
+        "initials": "KU",
+        "photo": "images/Marketing/1671315652187 - Kanika Upadhyay.jpg",
+        "linkedin": "https://www.linkedin.com/in/kanika-upadhyay-92208025b/",
+        "instagram": "https://instagram.com/not_kanika_upadhyay?igshid=MzNlNGNkZWQ4Mg=="
+      },
+      {
+        "name": "Kaustuv Devmishra",
+        "initials": "KD",
+        "photo": "images/Marketing/Kaustuv_Devmishra (1) - Kaustuv Devmishra.jpg",
+        "linkedin": "https://www.linkedin.com/in/kaustuv-devmishra-372868253/",
+        "instagram": "https://www.instagram.com/_kaustuv.d/"
+      },
+      {
+        "name": "Khwab Gupta",
+        "initials": "KG",
+        "photo": "images/Marketing/IMG_20230522_203640 - Khwab Gupta.jpg",
+        "linkedin": "https://www.linkedin.com/in/khwab-gupta",
+        "instagram": "https://www.instagram.com/khwabgupta?r=nametag"
+      },
+      {
+        "name": "Sahil Umale",
+        "initials": "SU",
+        "photo": "images/Marketing/pp-Sahil.jpg",
+        "linkedin": "/team",
+        "instagram": "https://www.linkedin.com/in/sahil-umale-03333025b"
+      },
+      {
+        "name": "Prathamesh Kawtikwar",
+        "initials": "PK",
+        "photo": "images/Marketing/profilepic - Prathamesh Kawtikwar.jpeg",
+        "linkedin": "https://www.linkedin.com/in/prathamesh-kawtikwar-1b204625b/",
+        "instagram": "https://www.instagram.com/prathamesh.k1709/"
+      },
+      {
+        "name": "Divya Singh Maurya",
+        "initials": "DM",
+        "photo": "images/Marketing/IMG_20230112_215651 - Divya Singh.jpg",
+        "linkedin": "https://www.linkedin.com/in/divya-singh-maurya-b04747220",
+        "instagram": "https://instagram.com/diva_sm_23?igshid=MzNlNGNkZWQ4Mg=="
+      },
+      {
+        "name": "Manvendra saini",
+        "initials": "MS",
+        "photo": "images/Marketing/Manvendra Saini.jpg",
+        "linkedin": "https://www.linkedin.com/in/manvendra-saini-285742250/",
+        "instagram": "https://www.instagram.com/_manvendra_saini_27/?next=%2F"
+      },
+      {
+        "name": "Gaurav kumar",
+        "initials": "GK",
+        "photo": "images/Marketing/IMG_20230608_133038 - Gaurav Kumar.jpg",
+        "linkedin": "https://www.linkedin.com/in/gaurav-kumar-696589254",
+        "instagram": "https://instagram.com/gaurav99503?igshid=OTk0YzhjMDVlZA=="
+      },
+      {
+        "name": "Arnav Jain",
+        "initials": "AJ",
+        "photo": "images/Marketing/pp - Arnav Nirmal.jpg",
+        "linkedin": "https://www.linkedin.com/in/arnav-jain2710/",
+        "instagram": "https://www.instagram.com/_arnavjain/"
+      },
+      {
+        "name": "Janhvi Verma",
+        "initials": "JV",
+        "photo": "images/Marketing/IMG_2133 - Janhvi Verma .jpg",
+        "linkedin": "/team/www.linkedin.com/in/janhvi-verma-3680",
+        "instagram": "/team/instagram.com/_.janhvi2101/"
+      },
+      {
+        "name": "Saifuddin NagriWala",
+        "initials": "SN",
+        "photo": "images/Marketing/DSC09278 (1) - Saifuddin NagriWala.jpg",
+        "linkedin": "http://linkedin.com/in/saifuddin-nagriwala-52104325b",
+        "instagram": "https://www.instagram.com/saifuddin_nw/"
+      },
+      {
+        "name": "S Sri Vaishnavi",
+        "initials": "SV",
+        "photo": "images/Marketing/20221217_164250 - S Sri.jpg",
+        "linkedin": "https://www.linkedin.com/in/vaishnavi-sunku-415631250",
+        "instagram": "https://instagram.com/vaishh_1035?igshid=ZDc4ODBmNjlmNQ=="
+      },
+      {
+        "name": "Prachi Patil",
+        "initials": "PP",
+        "photo": "images/Marketing/IMG_20230209_154838 - Prachi Patil.jpg",
+        "linkedin": "/team/ https://www.linkedin.com/in/prachi-patil-93402725b/",
+        "instagram": "https://instagram.com/prachi_patil0205?igshid=NGExMmI2YTkyZg=="
+      },
+      {
+        "name": "Hrishikesh Hiremath",
+        "initials": "HH",
+        "photo": "images/Marketing/Screenshot_20230611_123149_Gallery - Hrishikesh Hiremath.png",
+        "linkedin": "/team/ https://www.linkedin.com/in/hrishikesh-hiremath/",
+        "instagram": "https://instagram.com/hrishikesh_hiremath?igshid=MzNlNGNkZWQ4Mg=="
+      },
+      {
+        "name": "Harshita Bankure",
+        "initials": "HB",
+        "photo": "images/Marketing/WhatsApp Image 2023-06-07 at 19.39.47 - Harshita Bankure.jpeg",
+        "linkedin": "/team/ https://www.linkedin.com/in/harshita-bankure-37508525b/",
+        "instagram": "https://www.instagram.com/harshita_bankure/"
+      },
+      {
+        "name": "Shreya Itlod",
+        "initials": "SI",
+        "photo": "images/Marketing/InShot_20220815_202056386 - Itlod Shreya.jpg",
+        "linkedin": "/team/ https://www.linkedin.com/in/shreya-itlod-40b02825b?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BzXt2io9NTKCooDvAX42u1w%3D%3D",
+        "instagram": "https://www.instagram.com/__shri03/#"
+      },
+      {
+        "name": "Anshu Ratan",
+        "initials": "AR",
+        "photo": "images/Marketing/IMG_20230425_030940_689 - Anshu Ratan.jpg",
+        "linkedin": "/team/ https://www.linkedin.com/in/anshu-ratan-b617601bb",
+        "instagram": "https://instagram.com/anshu_ratan_1101?igshid=MzNlNGNkZWQ4Mg=="
+      }
+    ],
+    "Technicals": [
+      {
+        "name": "Devanshi Chhatbar",
+        "initials": "DC",
+        "photo": "images/Technicals/DevanshiC.jpg",
+        "linkedin": "https://www.linkedin.com/in/devanshi-chhatbar-07531825a/",
+        "instagram": "https://www.instagram.com/devanshi._.005/"
+      },
+      {
+        "name": "Prince Kumar",
+        "initials": "PK",
+        "photo": "images/Technicals/Prince_Kumar.jpg",
+        "linkedin": "https://www.linkedin.com/in/k07prince",
+        "instagram": "https://www.instagram.com/prince.__.kumar"
+      },
+      {
+        "name": "Akshat Sharma",
+        "initials": "AS",
+        "photo": "images/Technicals/Akshat_Sharma.jpg",
+        "linkedin": "https://www.linkedin.com/in/vishal-k-26b179231/",
+        "instagram": "/team"
+      },
+      {
+        "name": "Prateek Rajput",
+        "initials": "PR",
+        "photo": "images/Technicals/Prateek.jpg",
+        "linkedin": "https://www.linkedin.com/in/prateek-rajput-46b9ba287",
+        "instagram": "https://www.instagram.com/prateek.r.rajput/"
+      },
+      {
+        "name": "Vasudha Waman",
+        "initials": "VW",
+        "photo": "/images/Technicals/Vasudha_Suneel.png",
+        "linkedin": "https://www.linkedin.com/in/vasudha-waman-1a3755293",
+        "instagram": "https://www.instagram.com/vasudha_waman/"
+      },
+      {
+        "name": "Jai Pannu",
+        "initials": "JP",
+        "photo": "/images/Technicals/Jai_Pannu.jpeg",
+        "linkedin": "https://www.linkedin.com/in/jaipannu-iiti",
+        "instagram": "https://www.instagram.com/jaipannu08/"
+      },
+      {
+        "name": "Akanksha Prasad",
+        "initials": "AP",
+        "photo": "images/Technicals/Prasad_Akanksha.jpg",
+        "linkedin": "https://www.linkedin.com/in/akanksha-prasad-ab0376250/",
+        "instagram": "https://www.instagram.com/akanksha_prasad18/"
+      },
+      {
+        "name": "Ayush Khadkekar",
+        "initials": "AK",
+        "photo": "images/Technicals/Ayush_Khadkekar.jpg",
+        "linkedin": "https://www.instagram.com/ayush_khadkekar",
+        "instagram": "https://www.instagram.com/ayush_khadkekar"
+      }
+    ],
+    "Content and Social Media": [
+      {
+        "name": "Rishit Mehrotra",
+        "initials": "RM",
+        "photo": "images/Content/Screenshot_20230607_124520_Gallery - Rishit Mehrotra.jpg",
+        "linkedin": "https://www.linkedin.com/in/rishit-mehrotra-637488250",
+        "instagram": "https://instagram.com/rishit_mehrotra_?igshid=ZDc4ODBmNjlmNQ=="
+      },
+      {
+        "name": "Kanika Upadhyay",
+        "initials": "KU",
+        "photo": "images/Content/1671315652187 - Kanika Upadhyay.jpg",
+        "linkedin": "https://www.linkedin.com/in/kanika-upadhyay-92208025b/",
+        "instagram": "https://instagram.com/not_kanika_upadhyay?igshid=MzNlNGNkZWQ4Mg=="
+      },
+      {
+        "name": "Palak devdwal",
+        "initials": "PD",
+        "photo": "images/Content/IMG-20230620-WA0010 - Palak Devdwal.jpg",
+        "linkedin": "https://www.linkedin.com/in/palak-devdwal-166598270",
+        "instagram": "https://instagram.com/palak.devdwal?igshid=NGExMmI2YTkyZg=="
+      },
+      {
+        "name": "Saksham Gupta",
+        "initials": "SG",
+        "photo": "images/Content/IMG_20230527_145430_032 - Saksham Gupta.jpg",
+        "linkedin": "https://www.linkedin.com/in/saksham-gupta-2aa803251",
+        "instagram": "https://www.instagram.com/saksham__gupta__21/"
+      }
+    ],
+    "Outreach": [
+      {
+        "name": "Manan Nigam",
+        "initials": "MN",
+        "photo": "images/Outreach/manannigam.jpg",
+        "linkedin": "https://www.linkedin.com/in/manan-nigam-216690297/",
+        "instagram": "https://www.instagram.com/_manan._.07_/"
+      },
+      {
+        "name": "Lokery Kiranmayi",
+        "initials": "LK",
+        "photo": "images/Outreach/lokery.jpeg",
+        "linkedin": "https://www.linkedin.com/in/kiranmayi-lokery-812a50297/",
+        "instagram": "https://www.instagram.com/?hl=en"
+      },
+      {
+        "name": "Sai Chathura Alam",
+        "initials": "SA",
+        "photo": "images/Outreach/alam.jpg",
+        "linkedin": "/team/linkedin.com/in/sai-chathura-alam-0916b9309",
+        "instagram": "/team"
+      },
+      {
+        "name": "Abhijeet Parmar",
+        "initials": "AP",
+        "photo": "images/Outreach/abhijeet.jpg",
+        "linkedin": "https://www.linkedin.com/in/abhijeet-parmar-a08696281",
+        "instagram": "https://www.instagram.com/abhijeet_parmar_05"
+      },
+      {
+        "name": "Mahi Shah",
+        "initials": "MS",
+        "photo": "images/Outreach/mahi.jpg",
+        "linkedin": "https://www.linkedin.com/in/mahi-s-shah",
+        "instagram": "https://www.instagram.com/mahishahhhhh/"
+      },
+      {
+        "name": "Naman Goyal",
+        "initials": "NG",
+        "photo": "images/Outreach/naman.jpg",
+        "linkedin": "/team",
+        "instagram": "https://www.instagram.com/naman_goyal.04/"
+      },
+      {
+        "name": "Muhamed Nihal",
+        "initials": "MN",
+        "photo": "images/Outreach/MuhamedNihal.jpg",
+        "linkedin": "https://www.linkedin.com/in/muhamed-nihal-9a14a9289",
+        "instagram": "https://www.instagram.com/nlk505/"
+      },
+      {
+        "name": "Yash Singh",
+        "initials": "YS",
+        "photo": "images/Outreach/yash2.jpg",
+        "linkedin": "/team/www.linkedin.com/in/singhyashh",
+        "instagram": "https://www.instagram.com/singhyashhhhh/"
+      },
+      {
+        "name": "Bhavya Shah",
+        "initials": "BS",
+        "photo": "images/Outreach/bhavya.jpg",
+        "linkedin": "https://www.linkedin.com/in/lovely-bhardwaj-89603425b",
+        "instagram": "https://www.instagram.com/bhavya05shah/"
+      },
+      {
+        "name": "Mannan Mahajan",
+        "initials": "MM",
+        "photo": "images/Outreach/mananmahajan.jpg",
+        "linkedin": "http://www.linkedin.com/in/mannan-mahajan-875aa427b",
+        "instagram": "https://www.instagram.com/mm.mannan/?hl=en"
+      },
+      {
+        "name": "Hardik Bansal",
+        "initials": "HB",
+        "photo": "images/Outreach/hardik2.jpg",
+        "linkedin": "/team",
+        "instagram": "https://www.instagram.com/hardikbansal93/"
+      },
+      {
+        "name": "Ansh Kayl",
+        "initials": "AK",
+        "photo": "images/Outreach/kayl.jpg",
+        "linkedin": "https://www.linkedin.com/in/ansh-kyal-b6950427b",
+        "instagram": "https://www.instagram.com/ansh_kyal29/"
+      },
+      {
+        "name": "Varad Pendse",
+        "initials": "VP",
+        "photo": "images/Outreach/pendse.jpg",
+        "linkedin": "https://www.linkedin.com/in/varad-s-pendse/",
+        "instagram": "https://www.instagram.com/varadpendse/"
+      },
+      {
+        "name": "Atharvakant Chandorikar",
+        "initials": "AC",
+        "photo": "images/Outreach/atharv2.jpg",
+        "linkedin": "https://www.linkedin.com/in/atharvakant-chandorikar-657313291",
+        "instagram": "https://www.instagram.com/atharvakant_?igsh=OG9lbG9kNXMxMTEy"
+      },
+      {
+        "name": "Ayush Jindal",
+        "initials": "AJ",
+        "photo": "images/Outreach/ayush.jpg",
+        "linkedin": "https://www.linkedin.com/in/ayush-jindal-24b22b280",
+        "instagram": "https://www.instagram.com/_ayush_jindal_15/"
+      }
+    ],
+    "Creatives": [
+      {
+        "name": "Tejas Chaudhari",
+        "initials": "TC",
+        "photo": "images/Video_Production_Creative/WhatsApp Image 2023-06-03 at 13.27.05 - Tejas Anil.jpg",
+        "linkedin": "https://www.linkedin.com/in/tejas-chaudhari-650543250/",
+        "instagram": "https://instagram.com/tejas.chaudhari22?igshid=MzNlNGNkZWQ4Mg==="
+      },
+      {
+        "name": "Yash Yewale",
+        "initials": "YY",
+        "photo": "images/Video_Production_Creative/IMG_20230608_221535 - Yash Yewale.jpeg",
+        "linkedin": "https://www.linkedin.com/in/yash-yewale-29a453256",
+        "instagram": "https://instagram.com/yash_yewale163?igshid=OTk0YzhjMDVlZA=="
+      }
+    ]
+  }
+}
+
+export default TEAM_DATA;
