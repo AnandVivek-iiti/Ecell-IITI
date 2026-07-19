9833db68c25f92f4f157896b067b9623debf98d5
Harshk013 <harshkasera262@gmail.com>
2026-07-19 13:34:59 +0530

    Create event.js

---
+++ src/data/event.js
@@ -0,0 +1,50 @@
+
+
+export const events = [
+  {
+    id: 1,
+    title: "Marketing Fiesta",
+    date: "5th June",
+    image: "/assets/Events/Event 1.jpg",
+    shortDesc: "Test your marketing skills.",
+    fullDesc:
+      "The primary purpose of this particular competition was to facilitate a case study that was specifically designed to delve into the intricacies of marketing. The main aim was to explore and gain valuable insights and knowledge in the realm of marketing, which serves as the lifeblood of any successful business. Participants were tasked with putting their skills in the art of buying and selling to the test, as this competition served as a platform to assess their proficiency in this essential aspect of business operations.",
+  },
+  {
+    id: 2,
+    title: "Pitching Competition",
+    date: "12th June",
+    image: "/assets/Events/Event 2.jpeg",
+    shortDesc: "Refine your pitching skills.",
+    fullDesc:
+      "Marketing has evolved into one of the most crucial pillars for building brands in the past few years.Disruptive marketing strategies have the potential to completely transform market dynamics.To help our budding entrepreneurs learn exactly that,we held pitching competitions,for Fluxus 10.0.It helped students refine their marketing and persuasive skills in the form of a healthy and fun competition.It was aimed to make a subtle effort to help students learn the importance of belief and trust of an entrepreneur on his product.It was a fun and competitive event with products and pitches of all kinds !",
+  },
+  {
+    id: 3,
+    title: "Entrepreneur's Talks",
+    date: "18th June",
+    image: "/assets/Events/Event 3.jpeg",
+    shortDesc: "Listen to successful entrepreneurs.",
+    fullDesc:
+      "Talk shows remain the highlight of the summit each year,attracting a huge audience of students,professors and more from across different institutions.This year we aim to make the talk show grand and glorious than ever,and will be hosting some of the most celebrated entrepreneurs who are sure to leave you spellbound with their journeys and experiences.In the past editions,we have hosted entrepreneurs like Aman Dhattarwal, Sandeep Jain,Mayank Pratap and many more who have built their businesses from scratch and have brought revolution in their respective fields.We are excited to host yet another power packed edition to the talk show that will leave you energized to the core !",
+  },
+  {
+    id: 4,
+    title: "Startup Expo",
+    date: "25th June",
+    image: "/assets/Events/Event 4.jpeg",
+    shortDesc: "Showcase your startup ideas.",
+    fullDesc:
+      "A start up expo was organised that witnessed stalls of numerous interesting and innovative startups that had engaging display units.A lot of people came to interact with the founders and learnt how startups actually get going.The event was a huge success and the audience was left thoroughly impressed.",
+  },
+  {
+    id: 5,
+    title: "E-Summit 2025",
+    date: "2nd July",
+    image: "/assets/Events/Event 5.png",
+    shortDesc: "Learn about entrepreneurship and innovation.",
+    fullDesc:
+      "E-Summit is the flagship event organized by the E-Cell,IIT Indore annually.It is one of the most awaited events hosted by the institute that witnesses massive participation from across the country.Through this event,we aim to ignite the entrepreneur inside students by bringing them together to compete in various competitions exploring all aspects of entrepreneurship. We also host some of the most seasoned and celebrated entrepreneurs who enlighten our audience with their jaw-dropping journeys.",
+  },
+ 
+];
